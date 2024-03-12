0a59140828cd4efd3806d2bcb64d46801659e1ad
Fernando Damián Cabrera <fernando_cabrera2006@hotmail.com>
2024-03-12 19:47:21 -0300

    paso el proyecto a vite y corrijo prop de ItemListContainer

--- PreEntrega1+CabreraFernandoDamian/app/src/App.jsx
+++ PreEntrega1+CabreraFernandoDamian/app/src/App.jsx
@@ -6,8 +6,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer />
-      {"¡Bienvenidos!"}
+      <ItemListContainer greeting="¡Bienvenidos!" />
     </div>
   );
 }
